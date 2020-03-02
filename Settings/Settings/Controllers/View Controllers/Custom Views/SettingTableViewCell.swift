feae0e2252d6d082c401a73efa06fb11f0c7b995
Anthroman <30359515+Anthroman@users.noreply.github.com>
2020-03-02 13:19:25 -0700

    TableView set up

---
+++ Settings/Settings/Controllers/View Controllers/Custom Views/SettingTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  SettingTableViewCell.swift
+//  Settings
+//
+//  Created by Anthroman on 3/2/20.
+//  Copyright © 2020 Karl Pfister. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    //MARK: - Outlets
+    @IBOutlet weak var settingIconImageView: UIImageView!
+    @IBOutlet weak var settingNameLabel: UILabel!
+    @IBOutlet weak var settingSwitch: UISwitch!
+    
+    //MARK: - Actions
+    
+    @IBAction func settingSwitchToggled(_ sender: Any) {
+        //TOTO: - Add this
+    }
+    
+    //MARK: - Helper Methods
+    
+    func updateViews(with setting: Setting) {
+        settingIconImageView.image = setting.icon
+        settingNameLabel.text = setting.settingTitle
+        settingSwitch.isOn = setting.isOn
+        
+        backgroundColor = setting.isOn ? .cyan : .white
+        
+    }
+}
